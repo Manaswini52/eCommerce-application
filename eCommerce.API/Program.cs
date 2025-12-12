eda0e7b9cdf66852540a1680ea266b41922b7444
Manaswini Gorti <manaswini1052@gmail.com>
2025-12-12 15:25:27 +0530

    Initial commit - add DI

---
+++ eCommerce.API/Program.cs
@@ -0,0 +1,26 @@
+using eCommerce.Core;
+using eCommerce.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Add Infrastructure services
+builder.Services.AddInfrastructure();
+
+//Add Core services
+builder.Services.AddCore();
+
+//Add controllers to service collection
+builder.Services.AddControllers();
+
+//build the web app
+var app = builder.Build();
+
+//Routing
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseAuthentication();
+
+//enable routing for controllers
+app.MapControllers();
+app.Run();
---
+++ eCommerce.Core/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerce.Core
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddCore(this IServiceCollection services)
+        {
+            return services;
+        }
+
+    }
+}
