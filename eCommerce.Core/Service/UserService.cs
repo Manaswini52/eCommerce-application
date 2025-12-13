a9654ced65599d107de37b3ac75c8963b546e81b
Manaswini Gorti <manaswini1052@gmail.com>
2025-12-13 18:34:13 +0530

    Users Service

---
+++ eCommerce.Core/Service/UserService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eCommerce.Core.DTO;
+using eCommerce.Core.Entities;
+using eCommerce.Core.RepositoryContracts;
+using eCommerce.Core.ServiceContracts;
+
+namespace eCommerce.Core.Service
+{
+    internal class UserService : IUserService
+    {
+        private readonly IUsersRepository _usersRepository;
+        public UserService(IUsersRepository usersRepository)
+        {
+            _usersRepository = usersRepository;
+        }
+
+        public async Task<AuthenticationResponse?> Login(LoginRequest loginRequest)
+        {
+            ApplicationUser? user = await _usersRepository.GetUserByEmailAndPassword(loginRequest.Email, loginRequest.Password);
+            if (user == null)
+            {
+                return null;
+            }
+            return new AuthenticationResponse(user.UserID,user.Email, user.PersonName,user.Gender, "token", Success:true);
+        }
+
+        public async Task<AuthenticationResponse> Register(RegisterRequest registerRequest)
+        {
+            ApplicationUser user = new ApplicationUser()
+            {
+                PersonName = registerRequest.PersonName,
+                Email = registerRequest.Email,
+                Password = registerRequest.Password,
+                Gender = registerRequest.Gender.ToString(),
+            };
+            ApplicationUser? registeredUser = await _usersRepository.AddUser(user);
+            if (registeredUser == null) 
+            {
+                return null; 
+            }
+            return new AuthenticationResponse(registeredUser.UserID,registeredUser.Email,registeredUser.PersonName,registeredUser.Gender, "token", Success:true);
+        }
+    }
+}
---
+++ eCommerce.Core/ServiceContracts/IUserService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eCommerce.Core.DTO;
+
+namespace eCommerce.Core.ServiceContracts
+{
+    public interface IUserService
+    {
+        /// <summary>
+        /// Method to handle login use case and returns authentication response object that contains the login status
+        /// </summary>
+        /// <param name="loginRequest"></param>
+        /// <returns></returns>
+        Task<AuthenticationResponse> Login(LoginRequest loginRequest);
+
+        /// <summary>
+        /// Method to handle registration use case and return authentication response type that represents status of user registration
+        /// </summary>
+        /// <param name="registerRequest"></param>
+        /// <returns></returns>
+        Task<AuthenticationResponse> Register(RegisterRequest registerRequest);
+
+    }
+}
